3dc52ef4df8cabdf3faaac3fbfa91b149e5a86fb
mac <mac@macs-MacBook-Pro.local>
2018-03-10 18:03:26 -0700

    only styling left

--- index.js
+++ index.js
@@ -1,93 +1,114 @@
 $(document).ready(function() {
 
-var questionSet = [$("#questionOne"), $("#questionTwo"), $("#questionThree"), $("#questionFour"), $("#questionFive"), $("#questionSix"), $("#questionSeven"), $("#questionEight"), $("#questionNine"), $("#questionTen"), $("#you")];
-
-var rightCounter = 0;
-var wrongCounter = 0;
+var countdown = 15;
 var counter = 0;
+var end;
+var expire;
+var fin;
+var questionSet = [$("#questionOne"), $("#questionTwo"), $("#questionThree"), $("#questionFour"), $("#questionFive"), $("#questionSix"), $("#questionSeven"), $("#questionEight"), $("#questionNine"), $("#questionTen"), $("#you")];
+var rightCounter = 0;
+var timesUp;
+var wrongCounter = 0;
 
-
-
-
-
-    $(".guess").on("click", reveal); 
-    $("#start").on("click", go); 
-    $(".right").on("click", correct);
-    $(".wrong").on("click", incorrect);
-    $(".wrong").on("click", badGuess);
-    $(".right").on("click", goodGuess);
-    $(".guess").on("click", ultimate);
-    $(".guess").on("click", videotape);
-
-    function videotape() {
-        if (counter == 10) {
-            setTimeout(function() { $("#you").css("visibility", "visible"); }, 5000);
-
-        }
-    }
+$(".right").on("click", correct);
+$("#start").on("click", clock);
+$("#start").on("click", go); 
+$(".wrong").on("click", incorrect);
     
-    function ultimate() {
-        if (rightCounter > wrongCounter) {
-            $("#you").html("You Win");
-        }
-        else {
-            $("#you").html("You Lose");
-        }
-    }
-    
-    function badGuess() {
-        wrongCounter++;
-        $("#losses").html(wrongCounter);
-        counter++;
+function clock() {
+        
+        setInterval(function(){ countdown--; }, 1000);
         
         
+        setInterval(function(){ $("#timer").html(countdown); }, 1000);
+        
+         
     }
 
-    function goodGuess() {
+    function correct() {
+        $("#timer").remove();
+        setTimeout(function() { questionSet.shift(); }, 4975);
+        setTimeout(function() { (questionSet[0]).remove(); }, 4950);
+       
+        setTimeout(function() { questionSet[0].css("visibility", "visible"); }, 5000);
+        
+        setTimeout(function() { countdown = 16; }, 5000);
+        clearTimeout(fin);
+        clearTimeout(timesUp);
+        clearTimeout(expire);
+        $(".right").css("font-weight", "bold");
+        $(".result").html("You're Right!");
+        setTimeout(function() { $(".result").html(""); }, 5000);
+        setTimeout(function() { $(".right").css("font-weight", "normal"); }, 5000);
+        fin = setTimeout(function() { $(".result").html("Time's Up!"); }, 20000);
         rightCounter++;
         $("#wins").html(rightCounter);
         counter++;
-    }
+        clearTimeout(end);
+        timesUp = setTimeout(function() { $(".result").html("Time's Up!"); }, 20000);
+        expire =  setTimeout(function() { incorrect(); }, 20000);
 
-
-    function reveal() {
-        $(".right").css("font-weight", "bold");
-       
-        setTimeout(function() { questionSet.shift(); }, 4975);
-        setTimeout(function() { (questionSet[0]).remove(); }, 4950);
-        setTimeout(function() { $(".right").css("font-weight", "normal"); }, 5000);
-        setTimeout(function() { questionSet[0].css("visibility", "visible"); }, 5000);
-        setTimeout(function() { $(".result").html(""); }, 4950);
-
-    }
-
+        if (rightCounter < wrongCounter) {
+            $("#you").html("You Lose");
+        }
+        else if (rightCounter == wrongCounter) {
+            $("#you").html("You Tied"); 
+        }
+        else {
+            $("#you").html("You Win!");
+        }
         
+    } 
+
+   
+
     function go() {
         
         $("#scoreboard").css("visibility", "visible");
         questionSet[0].css("visibility", "visible");
         $("#start").remove();
+        timesUp = setTimeout(function() { $(".result").html("Time's Up!"); }, 15000);
+        end = setTimeout(function() { incorrect(); }, 15000);
+        
         }
 
-    function correct() {
-        $(".result").html("You're Right!");
-        
-        
     
-        
-        
-    } 
 
     function incorrect() {
+        $("#timer").remove();
+        setTimeout(function() { questionSet.shift(); }, 4975);
+        setTimeout(function() { (questionSet[0]).remove(); }, 4950);
+       
+        setTimeout(function() { questionSet[0].css("visibility", "visible"); }, 5000);
+        
+        setTimeout(function() { countdown = 16; }, 5000);
+        clearTimeout(timesUp);
+        clearTimeout(fin);
+        clearTimeout(expire);
+
+        $(".right").css("font-weight", "bold");
         $(".result").html("You're Wrong");
+        setTimeout(function() { $(".result").html(""); }, 5000);
+        setTimeout(function() { $(".right").css("font-weight", "normal"); }, 5000);
+        fin = setTimeout(function() { $(".result").html("Time's Up!"); }, 20000);
+        wrongCounter++;
+        $("#losses").html(wrongCounter);
+        counter++;
+        clearTimeout(end);
+        timesUp = setTimeout(function() { $(".result").html("Time's Up!"); }, 20000);
+        expire =  setTimeout(function() { incorrect(); }, 20000);
         
-        
-        
-        
+        if (rightCounter < wrongCounter) {
+            $("#you").html("You Lose");
+        }
+        else if (rightCounter == wrongCounter) {
+            $("#you").html("You Tied"); 
+        }
+        else {
+            $("#you").html("You Win!");
+        }
         
     }
+});
 
-   
     
-    
-       });
